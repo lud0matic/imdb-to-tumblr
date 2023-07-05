fe86354fe21773bf5215e4bf2f0d600186b7cb17
Gaston Abril Rotger <abril.rotger@gmail.com>
2023-07-05 02:05:13 -0300

    Initial commit 🎉

---
+++ README.md
@@ -0,0 +1,85 @@
+![Alt text](assets/images/SCR-20230620-rxju.png)
+
+# iMDb to Tumblr
+
+## Table of Contents
+
+- [iMDb to Tumblr](#imdb-to-tumblr)
+  - [Table of Contents](#table-of-contents)
+  - [About ](#about-)
+  - [Getting Started ](#getting-started-)
+    - [Prerequisites](#prerequisites)
+    - [Installing](#installing)
+  - [Usage ](#usage-)
+  - [Why? ¯\\\_(ツ)\_/¯](#why-_ツ_)
+
+## About <a name = "about"></a>
+
+Publish in your tumblr blog the poster of the movie that you want.
+
+## Getting Started <a name = "getting_started"></a>
+
+### Prerequisites
+
+This script needs the API of Tumblr and TMDB.
+
++ Who to get those APIs
+  + TMBD: [Create an account](https://www.themoviedb.org/signup) and follow this [easy steps](https://developer.themoviedb.org/docs/getting-started).
+  +  Tumblr: [Follow this steps](https://github.com/tumblr/pytumblr#using-the-interactive-console). `interactive_console.py` will create a `.tumblr` file in your root path.
+
+### Installing
+
+Clone this repo and install it wherever you want.
+
+`
+git clone https://github.com/lud0matic/imdb-to-tumblr.git
+`
+
+Rename `.env.exam` to `.env`
+
+Copy every API KEY that you get from the [Prerequisites](#prerequisites) and paste it in `.env` file. In `BLOG_NAME=` write the name of your blog <ins>without</ins> `tumblr.com`.
+
+If you use `poetry`, run.
+
+``` shell
+poetry install
+```
+
+Otherwise, use `pip`.
+
+```shell
+pip install -r requirements.txt
+```
+
+`Poetry` doesn't load `.env` variables by default. I installed [this plugin](https://github.com/volopivoshenko/poetry-plugin-dotenv) to solve this.
+
+If you end up using `pip`, install `python-dotenv`
+
+```shell
+pip install python-dotenv
+```
+
+And uncomment this two lines in main.py file.
+
+``` python
+from dotenv import load_dotenv
+load_dotenv()
+```
+
+## Usage <a name = "usage"></a>
+
+Find in [imdb.com](https://imdb.com) the movie poster that you want to puslish and copy the imdb id. In this case `tt6467226`.
+
+![Alt text](assets/images/SCR-20230703-rjxx.png)
+
+Run the script including the id. You will get the movie's name and the post url.
+
+![Alt text](assets/images/SCR-20230703-rluc.png)
+
+The title of the post will be the name of the movie, and the tags will include the original movie's name, its translation to english, the director's name and the current year. If you use the `-f` flag, the favorite tag will be included in the post. This helps to filter all the posts that you think deserve that category.
+
+![Alt text](assets/images/SCR-20230703-rlrn.png)
+
+## Why? ¯\\\_(ツ)_/¯ 
+
+Since 2011, I have been posting the poster of every movie I watched. It is one of the many things I love to keep track of. During the pandemic, I learned Python. Before that, every post I published [on my blog](https://mubiss.tumblr.com) was created manually. I would search for the poster, resize it, copy the movie's name and director, and then publish the post. At one point, I ended up hating this process. The main idea behind this script is to automate the entire process.
---
+++ main.py
@@ -0,0 +1,110 @@
+import requests, datetime, pytumblr, os, argparse, sys
+from PIL import Image
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+TUMBLR_CONSUMER_KEY = os.getenv("TUMBLR_CONSUMER_KEY")
+TUMBLR_CONSUMER_SECRET_KEY = os.getenv("TUMBLR_CONSUMER_SECRET_KEY")
+TUMBLR_OAUTH_TOKEN = os.getenv("TUMBLR_OAUTH_TOKEN")
+TUMBLR_OAUTH_SECRET = os.getenv("TUMBLR_OAUTH_SECRET")
+
+TMDB_API = os.getenv("TMDB_API")
+API_URL = "https://api.themoviedb.org/3/"
+IMAGE_URL = 'https://image.tmdb.org/t/p/original'
+
+BLOG_NAME = os.getenv("BLOG_NAME")
+
+IMAGE_HEIGHT = 500
+IMAGE_WIDTH = 700
+
+current_year = datetime.datetime.now()
+parser = argparse.ArgumentParser(description="Publish post on Tumblr using tmdb")
+parser.add_argument("IMDB_ID", action="store", help="Insert iMBd ID. Format: tt[ID]")
+parser.add_argument("-f", action="store_true",dest="fav",default=False, help="Tag movie as favourite")
+args = parser.parse_args()
+fav = args.fav
+
+###Tumblr###
+
+client = pytumblr.TumblrRestClient(TUMBLR_CONSUMER_KEY,
+                                   TUMBLR_CONSUMER_SECRET_KEY,
+                                   TUMBLR_OAUTH_TOKEN,
+                                   TUMBLR_OAUTH_SECRET)
+                                
+def request_movie():    
+    r = requests.get(API_URL + "find/" + IMDB_ID + "?api_key=" + TMDB_API + "&external_source=imdb_id")
+    json_list = r.json()
+    json_list = json_list.get("movie_results")
+
+    tags_dict = {}
+    try:
+        for k, v in json_list[0].items():
+            if k == "id":
+                tags_dict.update({k:v})
+            elif k == "title":
+                tags_dict.update({k:v})
+            elif k == "original_title":
+                tags_dict.update({k:v})
+            elif k == "poster_path":
+                tags_dict.update({k:v})
+        if args.fav == True:
+            tags_dict.update({"fav": "Fav"})
+    except IndexError:
+        print("Wrong ID. Try again") 
+        sys.exit(2)
+
+    return(tags_dict)
+
+def request_imagen_and_resizing():
+    r = requests.get(IMAGE_URL + request_movie().get("poster_path"))
+    filename = request_movie().get("poster_path").split('/')[-1]
+    file = open(filename, 'wb')
+    file.write(r.content)
+    file.close()
+    path = os.getcwd()
+    fullpath = (path + '/'+ filename)
+    #resizing
+    image = Image.open(fullpath)
+    new_image = image.resize((IMAGE_HEIGHT,IMAGE_WIDTH))
+    new_image.save(fullpath)
+
+    return(fullpath)
+
+def request_director():
+    r = requests.get(API_URL + "movie/" + str(request_movie().get("id")) + "/credits?api_key=" + TMDB_API)
+    json_list_credits = r.json()
+    json_list_credits = json_list_credits.get("crew")
+    director_list = []
+    for i in json_list_credits:
+        if i['job'] == 'Director':
+            director_list.append((i['name']))
+
+    return director_list    
+
+def tags():
+    tag_list = []
+    for k, v in request_movie().items():
+        if k == "title":
+            tag_list.append(v)
+        elif k == "fav":
+            tag_list.append(v)
+        elif k == "original_title":
+            tag_list.append(v)
+    tag_list.append(request_director()[0])
+    tag_list.append(str(current_year.year))
+
+    return(tag_list)
+
+def post_tumblr():
+    client.create_photo(BLOG_NAME, state="published",caption=request_movie().get("title"), tags=tags(), data=request_imagen_and_resizing())
+    os.remove(request_imagen_and_resizing())
+    print(request_movie().get("title"))
+    #Print the URL of you latest post
+    posts_url_dict = client.posts(BLOG_NAME)  
+    post_url = (posts_url_dict["posts"][0])
+    print(post_url["post_url"])
+    
+if __name__ == "__main__":
+    IMDB_ID = args.IMDB_ID
+    post_tumblr()
