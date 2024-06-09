4b46a059e3b1054fedbf5c75121a0e16ffc298fb
Gaston Abril Rotger <abril.rotger@gmail.com>
2024-06-09 19:44:17 -0300

    - Update main.py
    - Refactor the way that tag lists are created
    
    - Update the function to create a post
    
    - Fix an error where the title and link to the post are shown before the progress bar ends
    
    - Add try/except for better server error handling

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import requests, datetime, pytumblr, os, argparse, sys
+import requests
+import datetime
+import pytumblr
+import os
+import argparse
+import sys
 from PIL import Image
-from rich.progress import (BarColumn,
+from rich.progress import (
+    BarColumn,
     MofNCompleteColumn,
     Progress,
     TextColumn,
     TimeElapsedColumn,
-    TimeRemainingColumn)
- 
- ##Custom Progress Bar##
+    TimeRemainingColumn
+)
+
+# Custom Progress Bar
 progress_bar = Progress(
-    TextColumn("[blue]Posting..."),    
+    TextColumn("[blue]Posting..."),
     BarColumn(),
     TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
     MofNCompleteColumn(),
@@ -19,118 +26,129 @@ progress_bar = Progress(
     TimeRemainingColumn(),
 )
 
-total_iterations = 13
+total_iterations = 5
 
+# Environment Variables
 TUMBLR_CONSUMER_KEY = os.getenv("TUMBLR_CONSUMER_KEY")
 TUMBLR_CONSUMER_SECRET_KEY = os.getenv("TUMBLR_CONSUMER_SECRET_KEY")
 TUMBLR_OAUTH_TOKEN = os.getenv("TUMBLR_OAUTH_TOKEN")
 TUMBLR_OAUTH_SECRET = os.getenv("TUMBLR_OAUTH_SECRET")
-
 TMDB_API = os.getenv("TMDB_API")
+BLOG_NAME = os.getenv("BLOG_NAME")
+
 API_URL = "https://api.themoviedb.org/3/"
 IMAGE_URL = 'https://image.tmdb.org/t/p/original'
 
-BLOG_NAME = os.getenv("BLOG_NAME")
+IMAGE_HEIGHT = 700
+IMAGE_WIDTH = 500
 
-IMAGE_HEIGHT = 500
-IMAGE_WIDTH = 700
+current_year = datetime.datetime.now().year
 
-current_year = datetime.datetime.now()
 parser = argparse.ArgumentParser(description="Publish post on Tumblr using tmdb")
-parser.add_argument("IMDB_ID", action="store", help="Insert iMBd ID. Format: tt[ID]")
-parser.add_argument("-f", action="store_true",dest="fav",default=False, help="Tag movie as favourite")
+parser.add_argument("IMDB_ID", action="store", help="Insert IMDb ID. Format: tt[ID]")
+parser.add_argument("-f", action="store_true", dest="fav", default=False, help="Tag movie as favourite")
 args = parser.parse_args()
 fav = args.fav
+IMDB_ID = args.IMDB_ID
 
-###Tumblr###
+# Tumblr Client
+client = pytumblr.TumblrRestClient(
+    TUMBLR_CONSUMER_KEY,
+    TUMBLR_CONSUMER_SECRET_KEY,
+    TUMBLR_OAUTH_TOKEN,
+    TUMBLR_OAUTH_SECRET
+)
 
-client = pytumblr.TumblrRestClient(TUMBLR_CONSUMER_KEY,
-                                   TUMBLR_CONSUMER_SECRET_KEY,
-                                   TUMBLR_OAUTH_TOKEN,
-                                   TUMBLR_OAUTH_SECRET)
-
-
-def request_movie():    
-    r = requests.get(API_URL + "find/" + IMDB_ID + "?api_key=" + TMDB_API + "&external_source=imdb_id")
-    json_list = r.json()
-    json_list = json_list.get("movie_results")
-
-    tags_dict = {}
+def request_movie():
     try:
-        for k, v in json_list[0].items():
-            if k == "id":
-                tags_dict.update({k:v})
-            elif k == "title":
-                tags_dict.update({k:v})
-            elif k == "original_title":
-                tags_dict.update({k:v})
-            elif k == "poster_path":
-                tags_dict.update({k:v})
-        if args.fav == True:
-            tags_dict.update({"fav": "Fav"})
-    except IndexError:
-        print("Wrong ID. Try again") 
+        r = requests.get(API_URL + "find/" + IMDB_ID + "?api_key=" + TMDB_API + "&external_source=imdb_id")
+        r.raise_for_status()
+        json_list = r.json().get("movie_results", [])
+        if not json_list:
+            raise ValueError("No movie found for the given IMDb ID.")
+        
+        movie_info = json_list[0]
+        tags_dict = {
+            "id": movie_info.get("id"),
+            "title": movie_info.get("title"),
+            "original_title": movie_info.get("original_title"),
+            "poster_path": movie_info.get("poster_path")
+        }
+        if fav:
+            tags_dict["fav"] = "Fav"
+        progress.update(task, advance=1)
+        return tags_dict
+    except requests.RequestException as e:
+        print(f"HTTP error occurred: {e}")
+        sys.exit(1)
+    except ValueError as e:
+        print(e)
         sys.exit(2)
-    
-    progress.update(task, advance=1)
-    return(tags_dict)
 
-def request_imagen_and_resizing():
-    r = requests.get(IMAGE_URL + request_movie().get("poster_path"))
-    filename = request_movie().get("poster_path").split('/')[-1]
-    file = open(filename, 'wb')
-    file.write(r.content)
-    file.close()
-    path = os.getcwd()
-    fullpath = (path + '/'+ filename)
-    #resizing
-    image = Image.open(fullpath)
-    new_image = image.resize((IMAGE_HEIGHT,IMAGE_WIDTH))
-    new_image.save(fullpath)
-    
-    progress.update(task, advance=1)
-    return(fullpath)
+def request_image_and_resize(movie_info):
+    try:
+        r = requests.get(IMAGE_URL + movie_info["poster_path"])
+        r.raise_for_status()
+        filename = movie_info["poster_path"].split('/')[-1]
+        with open(filename, 'wb') as file:
+            file.write(r.content)
+        
+        # Resizing the image
+        image = Image.open(filename)
+        new_image = image.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+        new_image.save(filename)
+        progress.update(task, advance=1)
+        return filename
+    except requests.RequestException as e:
+        print(f"HTTP error occurred: {e}")
+        sys.exit(1)
+    except IOError as e:
+        print(f"Error handling image: {e}")
+        sys.exit(1)
 
-def request_director():
-    r = requests.get(API_URL + "movie/" + str(request_movie().get("id")) + "/credits?api_key=" + TMDB_API)
-    json_list_credits = r.json()
-    json_list_credits = json_list_credits.get("crew")
-    director_list = []
-    for i in json_list_credits:
-        if i['job'] == 'Director':
-            director_list.append((i['name']))
-    
-    progress.update(task, advance=1)
-    return director_list    
+def request_director(movie_id):
+    try:
+        r = requests.get(API_URL + "movie/" + str(movie_id) + "/credits?api_key=" + TMDB_API)
+        r.raise_for_status()
+        crew_list = r.json().get("crew", [])
+        directors = [person["name"] for person in crew_list if person["job"] == "Director"]
+        progress.update(task, advance=1)
+        return directors
+    except requests.RequestException as e:
+        print(f"HTTP error occurred: {e}")
+        sys.exit(1)
 
-def tags():
-    tag_list = []
-    for k, v in request_movie().items():
-        if k == "title":
-            tag_list.append(v)
-        elif k == "fav":
-            tag_list.append(v)
-        elif k == "original_title":
-            tag_list.append(v)
-    tag_list.append(request_director()[0])
-    tag_list.append(str(current_year.year))
-    
+def create_tags(movie_info, directors):
+    tag_list = [
+        movie_info.get("title"),
+        movie_info.get("original_title"),
+        directors[0] if directors else None,
+        str(current_year)
+    ]
+    if fav:
+        tag_list.append("Fav")
     progress.update(task, advance=1)
-    return(tag_list)
+    return [tag for tag in tag_list if tag]
 
 def post_tumblr():
-    client.create_photo(BLOG_NAME, state="published",caption=request_movie().get("title"), tags=tags(), data=request_imagen_and_resizing())
-    os.remove(request_imagen_and_resizing())
-    progress.update(task, advance=1)
-    print(request_movie().get("title"))
-    #Print the URL of you latest post
-    posts_url_dict = client.posts(BLOG_NAME)  
-    post_url = (posts_url_dict["posts"][0])
-    print(post_url["post_url"])
-        
+    movie_info = request_movie()
+    image_path = request_image_and_resize(movie_info)
+    directors = request_director(movie_info["id"])
+    tags = create_tags(movie_info, directors)
     
+    client.create_photo(BLOG_NAME, state="published", caption=movie_info.get("title"), tags=tags, data=image_path)
+    os.remove(image_path)
+    progress.update(task, advance=1)
+    
+    
+    posts_url_dict = client.posts(BLOG_NAME)
+    post_url = posts_url_dict["posts"][0].get("post_url")
+    return movie_info.get("title"), post_url
+
 if __name__ == "__main__":
     with progress_bar as progress:
         task = progress.add_task("[green]Posting...", total=total_iterations)
-        IMDB_ID = args.IMDB_ID        
-        post_tumblr()
+        movie_title, movie_url = post_tumblr()
+    # Print the movie name and link after the progress bar ends
+    print(f"{movie_title}")
+    print(f"{movie_url}")
